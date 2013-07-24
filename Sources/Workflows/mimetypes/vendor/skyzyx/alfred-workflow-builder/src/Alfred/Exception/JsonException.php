6b2bf1dfca77e99fd13b85dc2e835dc5b67864b8
hzlzh <hzlzh.dev@gmail.com>
2013-07-24 22:58:49 +0800

    add mimetypes

---
+++ Sources/Workflows/mimetypes/vendor/skyzyx/alfred-workflow-builder/src/Alfred/Exception/JsonException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Alfred JSON Exception
+ *
+ * @author Ryan Parman (http://ryanparman.com)
+ */
+
+namespace Alfred\Exception;
+
+use Alfred\Exception\Exception;
+
+class JsonException extends Exception {}
